2cb062183df1b31ecbafc22b6fa6e67522d12371
Sayurid Bautista Cruz <230770@utxicotepec.edu.mx>
2024-10-17 13:55:44 -0600

    Practica 07
    Se agregaron datos de series y se manipularon

--- JS/Ejercicio06.js
+++ JS/Ejercicio06.js
@@ -58,3 +58,163 @@ const fechaLocal1MX =fechaActual.toLocaleString('es-MX',
         console.log("Se registran temperaturas mas")
         console.log("Los animales conmienzan con la hibiernacion")
      }
+     console.log(`Dado que nacio en el año de 1992 soy de la generacion: ${asignaGeneracion(1992)}`);
+     console.log("%c4.- Manejo de excepciones  (TRY/CATCH))", style_console);
+     /**
+      * En algunas oraciones existiran errores que no son culpa del programa, si no del usuario , la red,
+      * el SO o incluso de un middlware., pero que si duda debemos controlar para evitar las fallas de ejecusion
+      *  
+      */
+     console.log("caso 1");
+     console.log("Intentamos dividir : 0/10, el resultado es: ");
+     try{                    //intenta
+        let result=0/10;     //dividir  entre un entero
+        console.log(result)
+     }
+     catch(error)
+     {
+        console.log("Ocurrio un  error: "+error.message);
+     }
+     console.log("caso 2");
+     console.log("Intentamos dividir : 10/0, el resultado es: ");
+     try{                              //intenta
+        let result=10/0;               //dividir  entre un entero
+        console.log(result)
+     }
+     catch(error)
+     {
+        console.log("Ocurrio un  error: "+error.message);
+     }
+     console.log("caso 3");
+     console.log("Intentamos dividir : a/10, el resultado es: ");
+     try{                                //intenta
+        let result="a"/10;               //dividir una letra entre un entero
+        console.log(result)
+     }
+     catch(error)
+     {
+        console.log("Ocurrio un  error: "+error.message);
+     }
+     console.log("caso 4");
+     console.log("Intentamos dividir: la variable : a/10, el resultado es: ");                       
+     try{                              //intenta
+        let result="a"/10;               //dividir  entre un entero
+        console.log(result)
+     }
+     catch(error)
+     {
+        console.log("Ocurrio un  error: "+ error.message);
+     }
+     console.log("caso 5");
+     console.log("Intentamos dividir el valor de la variable x/ el valor de la variabley,el resultado es:");
+     try{                              //intenta
+        let x=8, y=2, result= x/y;     //dividir una variable no definida entre 
+        console.log(result)
+     }
+     catch(error)
+     {
+        console.log("Ocurrio un error:"+error.message);
+     }
+     console.log("%c5.- Control de ciclos (BREAK/CONTINUA))", style_console);
+     /**
+      * En algunas ocaciones sera importante detener un civlo de mnera 
+      * abrubta para controlar casos especiales de un ciclo
+      */
+     console.log("Vamos a contar del uno al 10...")
+     for(let num=1; num<=10; num++)
+        console.log(num);
+     console.log("Ahora necesitamos que si llegas al 7 pares de contar... suponiendo que solo por hoy es de mala suerte")
+  for (num=1; num<=10;num++)
+     if(num==7)
+        break;
+     else
+     console.log(num);
+  console.log("ahora necesitamps que si llegas al 7 te saltes eese numero y continues")
+  for (num=1; num <=10; num++)
+  {
+     if(num==7)
+     {
+        continue;
+     }
+     console.log(num);
+  }
+  console.log("%c6.- Ciclo iterativo (FOR))", style_console);
+  //Recorre de manera iterativa (i), de incremental o decremental
+  console.log("Los dias de la semana son en orden ascendente son: ")
+  let dias=["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"]
+  for(let i=0; i<dias.length; i++)
+  console.log(dias[i])
+  console.log("Ahora vamos a imprimir los meses en orden desendente...")
+  const meses=["Enero","febrero","Marzo","abril","Mayo","junio","julio","Agosto","septiembre","octubre","noviembre","Diciembre"]
+  for(let i=meses.length-1; i>=0; i--)
+     console.log(meses[i]);
+  console.log("%c7.- Ciclo Condicionales (WIHILE)", style_console);
+  //Estos siclos(BUCKLE dependen de una condicion para continuar ejecutandose)
+  let finDeSemana=false;
+  let mensaje="";
+  let j=0;
+  //Usamos J para que coincida con el indice
+  while(j<dias.length){
+     switch(j){
+        case 0:
+           finDeSemana=true;
+           mensaje="Mormingoo...Hora de dormir";
+           break;
+        case 1:
+           finDeSemana=false;
+           mensaje="San lunes a trabajar";
+           break;
+        case 2:
+           finDeSemana=false;
+           mensaje="Segundo dia de chamba a darle";
+           break;
+        case 3:
+           finDeSemana=false;
+           mensaje="hombligo de semana";
+           break;
+        case 4:
+           finDeSemana=false;
+           mensaje="juevebes";
+           break;
+        case 5:
+           finDeSemana=false;
+           mensaje="vebiernes";
+           break;
+        case 6:
+           finDeSemana=false;
+           mensaje="sabatodo";
+           break;
+     }
+     //imprimir el nombre del dia y su mensaje 
+     if (!finDeSemana)
+     {
+        console.log(`Dia: ${dias[j]}`);
+        console.log(`Mensaje del dia: ${mensaje}`);
+     }
+     j++;
+     console.log("%c8.-Ciclo condicionales, que se ejecuta al menos una vez (DOWHILE)", style_console);
+     //Simulamos una lista de episodios de una temporada
+     let episodios=[
+        "Episodio 1:   hgh",
+        "Episodio 2:  ghghg ",
+        "Episodio 3: hghg",
+        "Episodio 4: ghgh",
+        "Episodio 5: hghg",
+     ];
+     let indice=0;
+     let continuarViendo= true; // Esta variable del usuario de continuar leyendo
+     
+     do {
+        console.log(`Reproduciendo${episodios[indice]}`);
+         //simulamos la reproduccion del pisodio
+       
+        indice++;
+         //simulamos una pregunta al usuario si desea seguir viendo
+       if (indice < episodios.length) {
+        continuarViendo = confirm("¿Deseas continuar con el siguiente episodio");
+       }else{
+        continuarViendo =false; ///Cuando se acaba de la lista de episodios 
+       }
+     }while (continuarViendo && indice < episodios.length);
+     console.log("Fin de la Reproducción.");
+  }
